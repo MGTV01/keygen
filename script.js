f9dee44bf48f3713a4cbc1b61c8a9b23d2bd5298
MGTV <125827172+MGTV01@users.noreply.github.com>
2024-09-12 08:56:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const MAX_KEYS_PER_GAME_PER_DAY = 50;
+    const MAX_KEYS_PER_GAME_PER_DAY = 100;
     //const EVENTS_DELAY = 50000;
 
-    const games = {
+      const games = {
         1: {
+            name: 'Zoopolls',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            timing: 30000,
+            attempts: 20,
+        },
+        2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            eventsDelay: 20000,
-            attemptsNumber: 10
+            timing: 30000,
+            attempts: 20,
         },
-        2: {
+        3: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
-        },
-        3: {
-            name: 'MergeAway',
-            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
+            timing: 30000, 
+            attempts: 15,
         },
         4: {
+            name: 'Merge Away',
+            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+            timing: 30000,
+            attempts: 25,
+        },
+        5: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            eventsDelay: 20000,
-            attemptsNumber: 10,
+            timing: 30000, 
+            attempts: 20,
         },
-         5 : {
+        6: {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
-        },
-        6: {
-            name: 'Mow and Trim',
-            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            timing: 20000, 
+            attempts: 20,
         },
         7: {
             name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            timing: 30000,
+            attempts: 20,
         },
         8: {
-            name: 'Zoopolis',
-            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            name: 'Mow and Trim',
+            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            timing: 30000,
+            attempts: 20,
         },
         9: {
             name: 'Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            timing: 30000,
+            attempts: 20,
         },
         10: {
             name: 'Stone Age',
             appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
             promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
-            eventsDelay: 20000,
-            attemptsNumber: 16,
+            timing: 20000,
+            attempts: 30,
+        },
+        11: {
+            name: 'Bouncemasters',
+            appToken: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+            promoId: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+            timing: 20000,
+            attempts: 30,
         }
     };
     
